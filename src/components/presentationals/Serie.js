250711d16a567380569e548edcc46ec4ee6969ee
Alexandre <a-palo@laposte.net>
2016-10-19 22:19:16 +0200

    serie component

---
+++ src/components/presentationals/Serie.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Serie extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render () {
+    return (
+      <tr>
+        <td>V</td>
+        <td>Répétitions: {this.props.serie.repetition}</td>
+        <td>Charge: {this.props.serie.load}</td>
+        <td onPress={() => { this.props.delete(this.props.serie.id) }}>X</td>
+      </tr>
+    )
+  }
+}
