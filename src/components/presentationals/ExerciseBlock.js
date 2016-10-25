caf944a6635b69f313331fcb59329405d7d17b77
Alexandre <a-palo@laposte.net>
2016-10-25 16:35:37 +0200

    refact presentational exerciseBlock

--- src/components/presentationals/ExerciseBlock.js
+++ src/components/presentationals/ExerciseBlock.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class ExerciseBlock extends Component {
-  // Connect: exerciseBlock, delete
-
-  constructor (props) {
-    super(props)
-    this.state = {
-    }
-  }
-
-  render () {
-    return (
-      <tr>
-        <td>V</td>
-        <td>Exercice: {this.props.exerciseBlock.exercise.name}</td>
-        <td>Séries: {this.props.exerciseBlock.series.length}</td>
-        <td onPress={() => { this.props.delete(this.props.exerciseBlock.id) }}>X</td>
-      </tr>
-    )
-  }
+const ExerciseBlock = ({
+  name,
+  numberSeries,
+  duration,
+  id
+}) => {
+  return (
+    <tr>
+      <td>V</td>
+      <td>{name}</td>
+      <td>{numberSeries}</td>
+      <td>{duration}</td>
+      <td onPress={() => onDelete(id)}>X</td>
+    </tr>
+  )
 }
+
+export default ExerciseBlock
