3018c68db1de7392c4644fa3c5b6b83e11ffd4cb
Alexandre <a-palo@laposte.net>
2016-11-05 15:55:32 +0100

    fix issue when deleting the current exerciseBlock

--- src/reducers/visibilityFilterExerciseBlock.js
+++ src/reducers/visibilityFilterExerciseBlock.js
@@ -2,6 +2,11 @@ const visibilityFilterExerciseBlock = (state = 0, action) => {
   switch (action.type) {
     case 'SET_VISIBILITY_FILTER_EXERCISEBLOCK':
       return action.filter
+    case 'REMOVE_EXERCISEBLOCK':
+      if (state === action.exerciseBlockId) {
+        return 0
+      }
+      return state
     case 'FINISH_EXERCISEBLOCK':
       return 0
     default:
