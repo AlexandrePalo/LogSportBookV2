c41069b1a8f31cb5f67646e51d91063b78c8b46e
Alexandre <a-palo@laposte.net>
2016-11-08 11:02:21 +0100

    basic routage

--- src/components/containers/Root.js
+++ src/components/containers/Root.js
@@ -1,11 +1,18 @@
 import { Provider } from 'react-redux'
-import React from 'react'
+import { Router, Route, browserHistory } from 'react-router'
+import React, { PropTypes } from 'react'
 import App from '../app'
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path='/' component={App} />
+    </Router>
   </Provider>
 )
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+}
 
 export default Root
