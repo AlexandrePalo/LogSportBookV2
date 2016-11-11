084b55f69bdcdd6e933b5abd12f4244da2f42cf8
Alexandre <a-palo@laposte.net>
2016-11-11 17:51:04 +0100

    logging with console.group

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,12 +3,36 @@ import { loadState, saveState } from './localStorage'
 import throttle from 'lodash/function/throttle'
 import rootReducer from './reducers/index'
 
+// Logging states and action
+const addLoggingToDispatch = (store) => {
+  const rawDispatch = store.dispatch
+  if (!console.group) {
+    return rawDispatch
+  }
+
+  return (action) => {
+    console.group(action.type)
+    console.log('%c prev state', 'color: gray', store.getState())
+    console.log('%c action', 'color: blue', action)
+    const returnValue = rawDispatch(action)
+    console.log('%c next state', 'color: green', store.getState())
+    console.groupEnd(action.type)
+    return returnValue
+  }
+}
+
+// Store
 const configureStore = () => {
   const persistedState = loadState()
   const store = createStore(
     rootReducer,
     persistedState
   )
+
+  // log
+  if (process.env.NODE_ENV !== 'production') {
+    store.dispatch = addLoggingToDispatch(store)
+  }
   store.subscribe(throttle(() => {
     saveState(store.getState())
   }, 1000))
