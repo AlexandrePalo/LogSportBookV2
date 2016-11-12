ddf6980faa47a8a1d0605d117098fad497b43ea1
Alexandre <a-palo@laposte.net>
2016-11-12 10:29:15 +0100

    use of redux-promise and redux-logger instead of custom middlewares

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,50 +1,21 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers/index'
-
-// Logging states and action
-const logger = (store) => (next) => {
-  if (!console.group) {
-    return next
-  }
-  return (action) => {
-    console.group(action.type)
-    console.log('%c prev state', 'color: gray', store.getState())
-    console.log('%c action', 'color: blue', action)
-    const returnValue = next(action)
-    console.log('%c next state', 'color: green', store.getState())
-    console.groupEnd(action.type)
-    return returnValue
-  }
-}
-
-// Accept promises
-const promiseSupporter = (store) => (next) => (action) => {
-  if (typeof action.then === 'function') {
-    return action.then(next)
-  }
-  return next(action)
-}
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.slice().reverse().forEach(middleware =>
-    store.dispatch = middleware(store)(store.dispatch)
-  )
-}
+import promise from 'redux-promise'
+import createLogger from 'redux-logger'
 
 // Store
 const configureStore = () => {
-  const store = createStore(
-    rootReducer
-  )
 
-  const middlewares = [promiseSupporter]
+  const middlewares = [promise]
   // log if not prod
   if (process.env.NODE_ENV !== 'production') {
-    middlewares.push(logger)
+    middlewares.push(createLogger())
   }
 
-  wrapDispatchWithMiddlewares(store, middlewares)
-
+  const store = createStore(
+    rootReducer,
+    applyMiddleware(...middlewares)
+  )
   return store
 }
 
